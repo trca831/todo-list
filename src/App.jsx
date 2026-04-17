e24202623e2a19f2de23b09c07661ac3ab1e4977
Tracy Cano <tracycano3.14@gmail.com>
2026-04-16 21:06:19 -0700

    lesson 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import TodoList from "./TodoList";
 import TodoForm from "./TodoForm";
 import "./App.css";
 
 function App() {
+  const [todoList, setTodoList] = useState(todos);
   return (
     <div>
       <h1>My Todo App</h1>
       <TodoForm />
-      <TodoList />
+      <TodoList todoList={todoList} />
     </div>
   );
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,5 +1,7 @@
-function TodoList() {
-  const todoList = [
+import TodoListItem from "./TodoListItem";
+
+function TodoList({ todoList }) {
+  const todos = [
     { id: 1, title: "review resources" },
     { id: 2, title: "take notes" },
     { id: 3, title: "code out app" },
@@ -7,7 +9,7 @@ function TodoList() {
   return (
     <ul>
       {todoList.map((todo) => (
-        <li key={todo.id}>{todo.title}</li>
+        <TodoListItem key={todo.id} todo={todo} />
       ))}
     </ul>
   );
